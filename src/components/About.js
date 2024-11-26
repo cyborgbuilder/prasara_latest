d530f1487b7738c6223fac855bbe1eed45face0d
cyborgeek10x <isuru@10xtek.com>
2024-11-26 17:07:17 +0530

    about us error

--- src/components/About.js
+++ src/components/About.js
@@ -548,37 +548,48 @@ const Down = styled.div`
 const Video = styled.div`
   width: 80%;
   height: 550px;
-  margin-bottom: 200px;
-  margin-top: 50px;
+  margin: 50px auto;
 
   h1 {
-    margin: 50px 0;
     text-align: center;
   }
 
-  @media only screen and (max-width: 1200px) {
+  iframe {
     width: 100%;
-    height: 300px;
-    margin-top: 0px;
-    margin-bottom: 150px;
+    height: 100%;
+  }
+
+  @media only screen and (max-width: 768px) {
+    height: 300px; /* Adjust iframe height */
+    margin: 20px 0;
   }
 `;
 
 const Title = styled.div`
   width: 90%;
   display: flex;
-  flex-wrap: wrap; /* Ensures wrapping */
-  justify-content: center;
+  align-items: center;
+  justify-content: space-between;
+  background: linear-gradient(#fff, #fff) padding-box,
+    linear-gradient(145deg, transparent 35%, var(--sec), var(--sec)) border-box;
+  border: 2px solid transparent;
   border-radius: 16px;
   margin: 40px 0;
-  padding: 10px;
+  box-shadow: 0px 187px 75px rgba(0, 0, 0, 0.01),
+    0px 105px 63px rgba(0, 0, 0, 0.05), 0px 47px 47px rgba(0, 0, 0, 0.09),
+    0px 12px 26px rgba(0, 0, 0, 0.1), 0px 0px 0px rgba(0, 0, 0, 0.1);
+
+  @media only screen and (max-width: 1200px) {
+    width: 95%;
+  }
 
   h1 {
-    font-size: 24px;
+    padding: 10px;
     color: var(--sec);
+    margin-left: 20px;
 
-    @media only screen and (max-width: 768px) {
-      font-size: 18px;
+    @media only screen and (max-width: 1200px) {
+      font-size: 24px;
     }
   }
 `;
