6439f4e357abdb6c6b4c8d5e5202ade1062d188f
cyborgeek10x <isuru@10xtek.com>
2024-11-26 18:00:08 +0530

    about us error

--- src/components/AwardsSection.js
+++ src/components/AwardsSection.js
@@ -1,7 +1,6 @@
 // File: AwardsSection.js
 import React from "react";
 import styled from "styled-components";
-import LazyImage from "./LazyImage";
 
 function AwardsSection() {
   const imgurl = "./award.png";
@@ -11,11 +10,7 @@ function AwardsSection() {
       {Array.from({ length: 20 }).map((_, i) => (
         <Slot key={i}>
           <Upper>
-            <LazyImage
-              src={imgurl}
-              blurhash="LGJH%5-O0b4_%#s+WENLNOR-x9xr"
-              alt=""
-            />
+            <img src={imgurl} alt={`Award ${i + 1}`} />
           </Upper>
           <Down>
             <h1>Sample Award {i + 1}</h1>
@@ -29,30 +24,31 @@ function AwardsSection() {
 const Awards = styled.div`
   width: 100%;
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
   gap: 20px;
   justify-content: center;
   padding: 20px;
 
   @media only screen and (max-width: 768px) {
-    grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+    padding: 10px;
   }
 `;
 
 const Slot = styled.div`
-  height: 300px;
+  height: 250px;
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
   align-items: center;
-  background-size: cover;
+  background-color: #f9f9f9;
   border-radius: 8px;
   box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
   position: relative;
   transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
 
   &:hover {
-    border: 1px solid #fff;
+    border: 1px solid #ddd;
     cursor: pointer;
     transform: scale(1.01);
     box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
@@ -65,14 +61,15 @@ const Slot = styled.div`
 `;
 
 const Upper = styled.div`
-  width: 50%;
+  width: 100%;
   height: 60%;
   display: flex;
   align-items: center;
   justify-content: center;
 
   img {
-    width: 100%;
+    width: 80%;
+    height: auto;
   }
 `;
 
@@ -81,7 +78,6 @@ const Down = styled.div`
   width: 100%;
   padding: 10px;
   background: #fff;
-  opacity: 0.8;
   border-radius: 8px;
   text-align: center;
 
